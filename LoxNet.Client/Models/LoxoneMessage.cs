0c0bc93260a1f07bd1d38d4bec10ceed58bcf54b
Kristoffer Tungland <kristoffer.tungland@live.no>
2025-07-09 19:04:41 +0200

    Treat all 2xx message codes as success

--- LoxNet.Client/Models/LoxoneMessage.cs
+++ LoxNet.Client/Models/LoxoneMessage.cs
@@ -5,11 +5,12 @@ using System.Text.Json;
 public record LoxoneMessage(int Code, JsonElement? Value, string? Message)
 {
     /// <summary>
-    /// Throws a <see cref="LoxoneApiException"/> if the message code is not 200.
+    /// Throws a <see cref="LoxoneApiException"/> if the message code is not
+    /// within the HTTP success range (200-299).
     /// </summary>
     public void EnsureSuccess()
     {
-        if (Code != 200)
+        if (Code < 200 || Code >= 300)
         {
             throw new LoxoneApiException(Code, Message);
         }
